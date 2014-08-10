b1a16be9a63f54e8487418b3564e9e6a0aa02831
scarytom <t.denley@cantab.net>
2014-08-10 11:14:40 +0100

    refactoring scoring logic

--- src/main/java/Arrangement.java
+++ src/main/java/Arrangement.java
@@ -1,45 +1,39 @@
-import java.util.Arrays;
 
 public class Arrangement {
+    private final RotatedPiece[] arrangement;
+
     public Arrangement(RotatedPiece[] arrangement) {
         this.arrangement = arrangement;
     }
 
-    private RotatedPiece[] arrangement = new RotatedPiece[9];
-
     public int score() {
         int score = 0;
         for (int row = 0; row < 3; row++) {
-            score += (arrangement.length > row * 3 + 1) ? canFitHorizontally(0 + row * 3) : 0;
-            score += (arrangement.length > row * 3 + 2) ? canFitHorizontally(1 + row * 3) : 0;
+            if (arrangement.length > row * 3 + 1 && canFitHorizontally(0 + row * 3)) {
+                score += 1;
+            }
+            if (arrangement.length > row * 3 + 2 && canFitHorizontally(1 + row * 3)) {
+                score += 1;
+            }
         }
         for (int piece = 0; piece < 6; piece++) {
-            if (arrangement.length > piece + 3) {
-                score += canFitVertically(piece);
+            if (arrangement.length > piece + 3 && canFitVertically(piece)) {
+                score += 1;
             }
         }
         return score;
     }
 
-    private int canFitHorizontally(int left) {
+    private boolean canFitHorizontally(int left) {
         return canFit(left, left + 1, 1, 3);
     }
 
-    private int canFitVertically(int top) {
+    private boolean canFitVertically(int top) {
         return canFit(top, top + 3, 2, 0);
     }
 
-    private int canFit(int piece1Idx, int piece2Idx, int face1, int face2) {
-        FaceDesign face1Design = arrangement[piece1Idx].face(face1);
-        FaceDesign face2Design = arrangement[piece2Idx].face(face2);
-        if (face1Design == face2Design) return 1;
-        else return 0;
+    private boolean canFit(int piece1Idx, int piece2Idx, int face1, int face2) {
+        return (arrangement[piece1Idx].face(face1) == arrangement[piece2Idx].face(face2));
     }
 
-    @Override
-    public String toString() {
-        return "Arrangement{" +
-                "arrangement=" + Arrays.toString(arrangement) +
-                "\n}";
-    }
 }
