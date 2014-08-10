cd3c83cf201a038a0a8bcddc021c4f7cc33acd98
scarytom <t.denley@cantab.net>
2014-08-10 10:01:31 +0100

    array of pieces in the puzzle

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -8,32 +8,15 @@ if none found, remove last piece and attempt with next
 
 public class Solver {
 
-    Piece piece1 = new Piece(new Type[] {
-            Type.Man, Type.WineBottle, Type.BarTender, Type.BeerGlass
-    });
-    Piece piece2 = new Piece(new Type[] {
-            Type.BeerGlass, Type.BarTender, Type.Man, Type.IHeartBeer
-    });
-    Piece piece3 = new Piece(new Type[] {
-            Type.BeerGlass, Type.Band, Type.IHeartBeer, Type.WineBottle
-    });
-    Piece piece4 = new Piece(new Type[] {
-            Type.IHeartBeer, Type.BeerGlass, Type.Man, Type.Band
-    });
-    Piece piece5 = new Piece(new Type[] {
-            Type.BarTender, Type.Band, Type.Man, Type.WineBottle
-    });
-    Piece piece6 = new Piece(new Type[] {
-            Type.BeerGlass, Type.BarTender, Type.Man, Type.WineBottle
-    });
-    Piece piece7 = new Piece(new Type[] {
-            Type.WineBottle, Type.BarTender, Type.IHeartBeer, Type.Band
-    });
-    Piece piece8 = new Piece(new Type[] {
-            Type.BarTender, Type.IHeartBeer, Type.BeerGlass, Type.Band
-    });
-    Piece piece9 = new Piece(new Type[] {
-            Type.Band, Type.Man, Type.WineBottle, Type.IHeartBeer
-    });
-
+    public static final Piece[] PIECES = new Piece[] {
+        new Piece(new Type[] {Type.Man, Type.WineBottle, Type.BarTender, Type.BeerGlass}),
+        new Piece(new Type[] {Type.BeerGlass, Type.BarTender, Type.Man, Type.IHeartBeer}),
+        new Piece(new Type[] {Type.BeerGlass, Type.Band, Type.IHeartBeer, Type.WineBottle}),
+        new Piece(new Type[] {Type.IHeartBeer, Type.BeerGlass, Type.Man, Type.Band}),
+        new Piece(new Type[] {Type.BarTender, Type.Band, Type.Man, Type.WineBottle}),
+        new Piece(new Type[] {Type.BeerGlass, Type.BarTender, Type.Man, Type.WineBottle}),
+        new Piece(new Type[] {Type.WineBottle, Type.BarTender, Type.IHeartBeer, Type.Band}),
+        new Piece(new Type[] {Type.BarTender, Type.IHeartBeer, Type.BeerGlass, Type.Band}),
+        new Piece(new Type[] {Type.Band, Type.Man, Type.WineBottle, Type.IHeartBeer})
+    };
 }
